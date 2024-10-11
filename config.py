b9c4b5e35f47e862faa43fe0c234aa52ddc28f5d
chailana <chailana35217793@gmail.com>
2024-10-11 13:07:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,4 +8,4 @@ class Config(object):
 
     API_HASH = os.environ.get("API_HASH", "")
 
-    STRING_SESSION = os.environ.get("STRING_SESSION", "")
+    STRING_SESSION = os.environ.get("STRING_SESSION", "1BVtsOH0Bu0SmIIde8ZsdV0GV1SGCnLtko8YvNwKJgavF3i93UmAVH6_EYtPd_THQ3zp3C4JIK3KwnRydJvVFMzEkfkiLMqes_yVJTyxobFmSvv7zrLIEHY75uCmLPhjEDhyM688KM1LwceYRwvIJI8-BFoIvFrMR5fTzBIQPF1J6dxBzIxRIEum7Jm6hwYS8mP7kvUG5C8OuaqXkEsYpplxgvfr8yj4U6hKNx19tJZggphG5PIU6KWwMPZYR-h8ymVmMXTRjov-Z8IcrHXJFjT17exDh9ax7gcLRytj1fH1kABdzbierblnDF31iM5DwIy0s5D4PlWM1nyaKw58UODuY-jIFnUc=")
